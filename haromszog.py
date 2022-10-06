32677a4f8868ca0f1bf4a8d8de4d4586635d7c3e
SzandraSzabina <99395337+SzandraSzabina@users.noreply.github.com>
2022-10-06 15:38:12 +0200

    initial

---
+++ haromszog.py
@@ -0,0 +1,6 @@
+def main():
+    side=int(input("Kérem a háromszög oldalának hosszát: "))
+    height=int(input("Kérem a háromszög magasságát: "))
+    print(f"A háromszög területe: {round(height*(side/2))}")
+
+main()
